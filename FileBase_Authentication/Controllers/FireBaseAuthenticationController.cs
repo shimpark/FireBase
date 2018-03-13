66d1cdfcb183119076bcf4c1fc504da037854032
Thuyvk's Blog <khanhthuy46k2dhv@gmail.com>
2018-03-13 15:52:30 +0700

    first commit

---
+++ FileBase_Authentication/Controllers/FireBaseAuthenticationController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+
+namespace FileBase_Authentication.Controllers
+{
+    public class FireBaseAuthenticationController : Controller
+    {
+        // GET: FireBaseAuthentication
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Firebase Email & Password Authentication
+        public ActionResult EmailPassword()
+        {
+            return View();
+        }
+
+        //Anonymous Authentication
+        public ActionResult Anonymous()
+        {
+            return View();
+        }
+
+        //Facebook Popup
+        public ActionResult FacebookPopup()
+        {
+            return View();
+        }
+    }
+}
---
+++ FileBase_Authentication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FileBase_Authentication.MvcApplication" Language="C#" %>
