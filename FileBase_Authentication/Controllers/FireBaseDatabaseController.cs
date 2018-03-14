15ecfe1ff5ed1ff28b25443ea8b672207277199a
Thuyvk's Blog <khanhthuy46k2dhv@gmail.com>
2018-03-14 11:12:38 +0700

    demo database

---
+++ FileBase_Authentication/Controllers/FireBaseDatabaseController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace FileBase_Authentication.Controllers
+{
+    public class FireBaseDatabaseController : Controller
+    {
+        // GET: FireBaseDatabase
+        public ActionResult Index()
+        {
+            ViewBag.Id = Guid.NewGuid();
+            return View();
+        }
+    }
+}
